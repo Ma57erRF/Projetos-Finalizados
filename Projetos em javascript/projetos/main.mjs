a57650ebd33a5355ac3f40a7812cb48450474451
Ma57erRF <ronaldfernandes2002@hotmail.com>
2022-06-21 22:22:51 -0300

    Adicionado: sitema de estacionamento criado com uso de Typescript e html segundo orientações de intrutor da Dio

---
+++ Projetos em javascript/projetos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade,mostraIdade,mostraHobby } from "./funçoes.mjs"
+
+console.log (mostraIdade('Ronald',32));
+console.log (mostraCidade('Ronald','São Gonçalo'));
+console.log (mostraHobby('Ronald','Jogar Games'));
